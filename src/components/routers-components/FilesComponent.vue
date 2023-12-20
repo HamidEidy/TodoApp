248cc56b40f26edfa27c27ea60008f1b2cf8f7f9
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-12-20 16:50:06 +0330

    update

--- src/components/routers-components/FilesComponent.vue
+++ src/components/routers-components/FilesComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="file" >
 <div id="content" onclick="document.getElementById('buy').style.display='block'">
-    <img src="./../../assets/Businessman Discussing Progress.png" alt="">
+    <!-- <img src="../../assets/Businessman Discussing Progress.png" alt=""> -->
     <p>Upgrade Your Account</p>
 </div>
   </div>
